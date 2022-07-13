2427d266e0005aac67a0a9cc2463d546d5864f68
Max Breitenfeldt <git@mxbr.me>
2022-07-13 16:42:23 +0200

    use highp for swirl

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -11,6 +11,7 @@ uniform float amount;
 
 #ifdef Swirl
 // Some useful functions
+precision highp float;
 vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
@@ -92,6 +93,7 @@ void effect(float intensity)
     offset = mix(offset, 0.0, intensity);
     FragColor = texture(movieTexture, TexCoords + offset);
 }
+precision mediump float;
 #endif
 
 #ifdef Blur
